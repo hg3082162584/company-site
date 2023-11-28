f13e8b52b244b407d6764a785ea2207061372785
MS QQ <314285141@qq.com>
2023-11-28 14:57:49 +0800

    Update Index.vue

--- views/Admin/Index.vue
+++ views/Admin/Index.vue
@@ -167,8 +167,8 @@ export default {
         category: null,
         seq: 1,
         image: null,
-        createUserId: "001134",
-        modifyUserid: "001134",
+        createUserId: "000",
+        modifyUserid: "000",
       },
       options: {
         // headers: {
